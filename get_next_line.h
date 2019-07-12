bf0f0ee2a0f19859bb1f0697f84dfb92557c0946
Pontsho MOGWERE <pmogwere@c5r5s7.wethinkcode.co.za>
2019-07-12 14:38:48 +0200

    My complete Get_Next_Line

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmogwere <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/06 16:18:12 by pmogwere          #+#    #+#             */
+/*   Updated: 2019/07/12 14:37:47 by pmogwere         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft/libft.h"
+# include <fcntl.h>
+
+# define BUFF_SIZE 32
+
+int		get_next_line(int const fd, char **line);
+#endif
